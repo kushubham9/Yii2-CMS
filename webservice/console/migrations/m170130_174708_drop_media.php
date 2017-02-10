5caa7e134f3b73c4dbc305ca6acc5146da36a922
Shubham <shubham@automitra.com>
2017-02-11 02:02:36 +0530

    imagemanger2

--- webservice/console/migrations/m170130_174708_drop_media.php
+++ webservice/console/migrations/m170130_174708_drop_media.php
@@ -11,6 +11,13 @@ class m170130_174708_drop_media extends Migration
 
     public function down()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+
         $this->createTable('media',
             ['id'=>$this->primaryKey(),
                 'media_type' => $this->string()->notNull(),
