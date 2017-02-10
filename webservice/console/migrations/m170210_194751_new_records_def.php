9f35e6ceae82c171ddd32db12fdd21148ce53e5d
Shubham <shubham@automitra.com>
2017-02-11 01:29:05 +0530

    New Changes

---
+++ webservice/console/migrations/m170210_194751_new_records_def.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m170210_194751_new_records_def extends Migration
+{
+
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+        $this->insert('category',['name'=>'Uncategorized', 'slug'=>'uncategorized', 'description'=>'Default Category', 'created_at'=> new Expression('NOW()'),
+                                        'updated_at'=> new Expression('NOW()')]);
+        $this->insert('user',['username'=>'Administrator', 'password_hash'=>'$2y$13$6yoLjvVORp/7EO1u8phYTuWYzhMSM4LVVsebZgcqEKj/EQLvo5nJK',
+                                'email'=>'animesh@automitra.com',
+                                'created_at'=> new Expression('NOW()'),
+                                'updated_at'=> new Expression('NOW()'),
+                                'status'=>1
+                            ]
+                    );
+    }
+
+    public function safeDown()
+    {
+        $this->delete('category',['slug'=>'uncategorized']);
+        $this->delete('user',['username'=>'Administrator']);
+    }
+
+}
